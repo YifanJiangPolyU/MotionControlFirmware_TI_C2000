92dc7e5b391801e3ed6245587edb98faec4cf23d
YifanJiangPolyU <jiang.uhrmacher@gmail.com>
2017-06-12 23:55:35 +0200

    add CommutationMaster private variables

--- MotionControl_Firmware_CPU1/Source/inc/CommutationMaster.h
+++ MotionControl_Firmware_CPU1/Source/inc/CommutationMaster.h
@@ -18,6 +18,40 @@
 #ifndef COMMUTATION_MASTER_H
 #define COMMUTATION_MASTER_H
 
+#include "stdint.h"
+#include "F28x_Project.h"
 
+class CommutationMaster{
+
+  public:
+    CommutationMaster():
+      NPolePair(0),
+      CountPerRev(8000),
+      HallSensorPattern(0b001),
+      CommAnglePerCount(0.0f),
+      CommAngle(0.0f),
+      CommAngle_Cos(1.0f),
+      CommAngle_Sin(0.0f)
+    {};
+
+    ~CommutationMaster(){};
+
+    void update(void);
+
+  private:
+    int32_t PosCounter_new;           // position counter value
+    int32_t PosCounter_old;
+
+    uint16_t  NPolePair;              // number of pole pairs (rotary)
+    uint16_t  CountPerRev;            // resolution: count per revolution (rotaty)
+
+    uint16_t  HallSensorPattern;      // hall sensor pattern (used only when hall sensor available)
+    float32_t CommAnglePerCount;      // electrical angle per count (rad)
+    float32_t CommAngle;              // electrical angle (rad)
+    float32_t CommAngle_Cos;          // cosine of electrical angle
+    float32_t CommAngle_Sin;          // sine of electrical angle
+
+
+};
 
 #endif
--- MotionControl_Firmware_CPU1/Source/src/CommutationMaster.cpp
+++ MotionControl_Firmware_CPU1/Source/src/CommutationMaster.cpp
@@ -16,3 +16,8 @@
 */
 
 #include "CommutationMaster.h"
+
+__attribute__((ramfunc))
+void CommutationMaster::update(void){
+
+}
